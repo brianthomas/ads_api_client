d5d5ca6dd1f4ccb0f93fc403be6a24fe26a04cb5
Brian Thomas <brian.a.thomas@nasa.gov>
2019-12-18 14:08:55 -0500

    add argparsing

--- bin/get_abstracts_by_year.py
+++ bin/get_abstracts_by_year.py
@@ -1,13 +1,11 @@
-
-#from urllib.parse import urlencode, quote_plus
-#query = {"author":"martínez neutron star"}
-#encoded_query = urlencode(query,quote_via=quote_plus)
-#print(encoded_query)
-
-import requests
+import argparse
 import json
+import logging
+import requests
 
-Token = 'iOTlKvECB8EqNWltL4WafhSrtLiplpdassiBuAkD' 
+LOG = logging.getLogger('ads_api_client')
+logging.basicConfig(level=logging.INFO)
+
 
 def papers_by_year (api_token:str, year:int, rows:int=10, start:int=0)->str:
     """ Call the ADS API and get all of the bibcodes for year """
@@ -31,7 +29,7 @@ def pull_all_papers_by_year (api_token:str, year:int)->str:
 
     paper_ids = []
 
-    data = papers_by_year(Token, year, 10)
+    data = papers_by_year(api_token, year, 10)
     print (data)
     print (data['response']['numFound'])
     paper_ids = paper_ids + data['response']['docs']
@@ -39,4 +37,19 @@ def pull_all_papers_by_year (api_token:str, year:int)->str:
     return paper_ids
 
 
-print (pull_all_papers_by_year(Token, 1970))
+
+if __name__ == '__main__':  # use if csv of text
+
+    ap = argparse.ArgumentParser(description='pii detection from text held in a file.')
+    ap.add_argument('-d', '--debug', default = False, action = 'store_true', help='Turn on debugging messages')
+    ap.add_argument('-t', '--token', type=str, help='ADS API Token to use.', required=True)
+    ap.add_argument('-y', '--year', type=int, help='Year to pull papers for.', required=True)
+
+    args = ap.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+        LOG.setLevel(logging.DEBUG)
+
+    print (pull_all_papers_by_year(args.token, args.year))
+
