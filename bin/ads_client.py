336aa2c40b8e65402e1322807323db0d2a3b40e8
Brian Thomas <brian.a.thomas@nasa.gov>
2020-01-08 12:44:18 -0500

    add pretty print option and add affiliation and acknowledgement fields as part of default fields to pull

--- bin/ads_client.py
+++ bin/ads_client.py
@@ -57,7 +57,8 @@ if __name__ == '__main__':  # use if csv of text
 
     ap = argparse.ArgumentParser(description='Client to pull data about ADS papers by year.')
     ap.add_argument('-d', '--debug', default = False, action = 'store_true', help='Turn on debugging messages')
-    ap.add_argument('-f', '--fields', type=str, default="abstract,arxiv_class,alternate_bibcode,bibcode,bibstem,citation_count,database,keyword,title", help='ADS paper fields to pull.', required=False)
+    ap.add_argument('-f', '--fields', type=str, default="abstract,ack,aff,aff_id,arxiv_class,alternate_bibcode,bibcode,bibstem,citation_count,database,keyword,title", help='ADS paper fields to pull.', required=False)
+    ap.add_argument('-p', '--pretty_print', default = False, action = 'store_true', help='Turn on pretty printed JSON output.')
     ap.add_argument('-t', '--token', type=str, help='ADS API Token to use.', required=True)
     ap.add_argument('-y', '--year', type=int, help='Year to pull papers for.', required=True)
 
@@ -70,5 +71,8 @@ if __name__ == '__main__':  # use if csv of text
     papers = pull_all_papers_by_year(args.token, args.year, args.fields)
 
     #printing JSON to stdout
-    print (json.dumps(papers))
+    if args.pretty_print:
+        print (json.dumps(papers, indent=2, sort_keys=True))
+    else:
+        print (json.dumps(papers))
 
